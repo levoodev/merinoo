81f7c72ab6c70a1be92f5bd496775d737e23ea4c
LM <merino.levy@gmail.com>
2021-02-09 17:53:25 -0600

    Updates

---
+++ precache-manifest.77075dcf7830d393edc37cc8da0983f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ef70e9a3e8363c63caf90b4f99c161",
+    "url": "/merinoo/index.html"
+  },
+  {
+    "revision": "1a9cf4dcbdf84da49b15",
+    "url": "/merinoo/static/css/main.8b4d56c5.chunk.css"
+  },
+  {
+    "revision": "e5fef107c4f214d3f195",
+    "url": "/merinoo/static/js/2.78005328.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/merinoo/static/js/2.78005328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a9cf4dcbdf84da49b15",
+    "url": "/merinoo/static/js/main.2b9d56c9.chunk.js"
+  },
+  {
+    "revision": "6271532139cb03135df3",
+    "url": "/merinoo/static/js/runtime-main.7990162b.js"
+  },
+  {
+    "revision": "085040fbb8e3d0a96eb4b39a865d584a",
+    "url": "/merinoo/static/media/Ellos.085040fb.jpg"
+  },
+  {
+    "revision": "6e946e0bffdd62f38d86e46cd6dc6783",
+    "url": "/merinoo/static/media/Izquierda.6e946e0b.jpg"
+  },
+  {
+    "revision": "cc8b3beb8ac1778929c83f0a64335348",
+    "url": "/merinoo/static/media/banner1.cc8b3beb.jpg"
+  }
+]);
